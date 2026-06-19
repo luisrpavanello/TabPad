80c268f11c9431a3a9aecaccf0656ba1ab1c1cf3
Luis Pavanello <luisrpavanello@gmail.com>
2026-06-19 20:03:14 -0300

    Fix commit

--- artifacts/editor/src/App.tsx
+++ artifacts/editor/src/App.tsx
@@ -1,6 +1,6 @@
 // artifacts/editor/src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Layout } from "@/components/Layout-new";
+import { Layout } from "@/components/Layout";
 import Editor from "./Editor";
 import Contact from "@/pages/Contact";
 import NotFound from "@/pages/not-found";
